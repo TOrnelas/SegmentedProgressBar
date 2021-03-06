83b84aa2d69dad2041237c1343ac0bce1c0c433a
Tiago Ornelas <tiagoornelas89@gmail.com>
2021-03-06 00:24:14 +0000

    Added listener to sample app

--- app/src/main/java/pt/tornelas/segmentedprogressbar/standard/StandardExampleActivity.kt
+++ app/src/main/java/pt/tornelas/segmentedprogressbar/standard/StandardExampleActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_standard_example.*
 import kotlinx.android.synthetic.main.layout_pager.*
 import pt.tornelas.segmentedprogressbar.R
+import pt.tornelas.segmentedprogressbar.SegmentedProgressBar
+import pt.tornelas.segmentedprogressbar.SegmentedProgressBarListener
 import pt.tornelas.segmentedprogressbar.dataSource
 import pt.tornelas.segmentedprogressbar.pager.PagerAdapter
 
@@ -15,13 +17,25 @@ class StandardExampleActivity : AppCompatActivity() {
         setContentView(R.layout.activity_standard_example)
 
         val items = dataSource()
+
         viewPager.adapter = PagerAdapter(supportFragmentManager, items)
         spb.viewPager = viewPager
+
         spb.segmentCount = items.size
+        spb.listener = object : SegmentedProgressBarListener {
+            override fun onPage(oldPageIndex: Int, newPageIndex: Int) {
+                // New page started animating
+            }
+
+            override fun onFinished() {
+                finish()
+            }
+        }
+
+        val spb = findViewById<SegmentedProgressBar>(R.id.spb)
         spb.start()
 
         btnNext.setOnClickListener { spb.next() }
         btnPrevious.setOnClickListener { spb.previous() }
-
     }
 }
